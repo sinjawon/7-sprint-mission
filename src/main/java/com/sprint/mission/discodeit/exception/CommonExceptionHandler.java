5b032c081bdcb944e69594e0f4344522c4ec6e1b
sinjawon <qlqhroal@naver.com>
2025-12-23 17:31:21 +0900

    `CommonExceptionHandler` 로깅 추가 (`@Slf4j`, `log.warn`)

--- src/main/java/com/sprint/mission/discodeit/exception/CommonExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/CommonExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.discodeit.exception;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
 
+@Slf4j
 @RestControllerAdvice
 public class CommonExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleException(IllegalArgumentException e) {
+        log.warn("잘못된 요청: {}", e.getMessage(), e);
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(e.getMessage());
@@ -20,6 +23,7 @@ public class CommonExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<String> handleException(NoSuchElementException e) {
+        log.warn("잘못된 요청: {}", e.getMessage(), e);
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(e.getMessage());
@@ -27,6 +31,7 @@ public class CommonExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
+        log.warn("잘못된 요청: {}", e.getMessage(), e);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(e.getMessage());
